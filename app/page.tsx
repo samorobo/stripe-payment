e54b136f3e6cc1d3244354788777458ee6f4ec5d
samorobo <samgodwin111@gmail.com>
2025-01-14 14:46:06 +0100

    My overall commit

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <div>
             <Hero />
               <Newest /> 
-            
+             <TrainingPay />
              
     {/* <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
       <div className="mb-10">
